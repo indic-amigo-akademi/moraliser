c507c6f9adbcfd42b93a4faf41ecc5a4d6bb92ea
Andre1998Shuvam <shuvamghosal98@gmail.com>
2021-03-01 12:42:02 +0530

    some db operations done

---
+++ README.md
@@ -0,0 +1,14 @@
+# Moraliser
+
+A simple chat software with adult and slang filter
+
+## Tags
+
+- "Lojja korena"
+- "Ainai giye bol beta"
+- "Bolis kire"
+- "Abar bol"
+- "Pitiye debo"
+- "Oslil"
+- "Eh"
+- "Yeaman"
--- server/chatDB.py
+++ server/chatDB.py
@@ -1,22 +1,58 @@
 import sqlite3
 import json
 '''
-TODO: createTable | deleteTable | insertData | updateData | deleteData | alterTable
+TODO: updateData | deleteData | alterTable
 '''
 
 
 class ChatDB():
     def __init__(self, filename="chat.db"):
-        self.createDB(filename)
+        self.__createDB(filename)
 
-    def createDB(self, filename='chat.db') -> None:
+    def __createDB(self, filename='chat.db') -> None:
         self.conn = sqlite3.connect(filename)
         self.cur = self.conn.cursor()
 
-    def createTable(self, tablename, columns):
+    def close(self):
+        self.cur.close()
+        self.conn.close()
+
+    def createTable(self, tablename, columns) -> None:
+        forList = []
+        defList = []
+        for col in columns:
+            defQuery, forQuery = col.getQuery()
+            defList.append(defQuery)
+            if forQuery is not None:
+                forList.append(forQuery)
+
         query = f'''
-        CREATE TABLE IF NOT EXISTS {tablename}
+        CREATE TABLE IF NOT EXISTS {tablename} (
+            {",".join(defList)}
+            {",".join(forList)}
+        )
         '''
+        print(query)
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def dropTable(self, tablename) -> None:
+        query = f''' DROP TABLE {tablename} '''
+        print(query)
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def insertData(self, tablename, values: dict):
+        col_query = ", ".join(values.keys())
+        val_query = ", ".join(['?' for i in range(len(values.values()))])
+        query = f'''INSERT INTO {tablename}({col_query}) VALUES ({val_query})'''
+        print(query)
+        self.cur.execute(query, list(values.values()))
+        self.conn.commit()
+
+    def updateData(self):
+        query = ''
+        print(query)
 
 
 '''
@@ -29,7 +65,7 @@ class DBColumn():
         validTypes = ['INTEGER', 'TEXT', 'BLOB', 'NUMERIC', 'REAL']
         self._name = _name
         self._type = _type if _type.upper() in validTypes else 'INTEGER'
-        self._nullable = False
+        self._notnullable = False
         self._autoincrement = False
         self._unique = False
         self._primary = False
@@ -47,8 +83,8 @@ class DBColumn():
         self._autoincrement = True
         return self
 
-    def setNullable(self):
-        self._nullable = True
+    def setNotNullable(self):
+        self._notnullable = True
         return self
 
     def setForeign(self, tablename, col):
@@ -66,8 +102,23 @@ class DBColumn():
         return f''' {{ 
             name : {self._name.__repr__()}, 
             type : {self._type.__repr__()}, 
-            nullable : {self._nullable.__repr__()}, 
+            nullable : {self._notnullable.__repr__()}, 
             autoincrement : {self._autoincrement.__repr__()}, 
             unique : {self._unique.__repr__()},
             foreign : {foreign}
         }}'''
+
+    def getQuery(self) -> tuple:
+        defQuery = f"{self._name} {self._type} { 'NOT NULL' if self._notnullable else ''} {'PRIMARY KEY' if self._primary else ''} {'AUTOINCREMENT' if self._autoincrement else ''} {'UNIQUE' if self._unique else ''}"
+        forQuery = f"FOREIGN KEY({self._name}) REFERENCES {self._foreign['table']}({self._foreign['col']})" if self._foreign else None
+        return (defQuery, forQuery)
+
+
+# chatDatabase = ChatDB()
+# columns = [
+#     DBColumn('id', 'INTEGER').setAutoincrement().setPrimary().setNotNullable(),
+#     DBColumn('username', 'TEXT').setUnique().setNotNullable()
+# ]
+# chatDatabase.createTable('users', columns)
+# chatDatabase.insertData('users', {"username": "shuvamogezel"})
+# chatDatabase.close()
