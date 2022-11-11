cc6cba4eb8c6348d0e2402b64aa5626bd243b9e5
Purbayan Chowdhury <pur.cho.99@gmail.com>
2022-11-11 23:30:51 +0530

    Added MButton component

---
+++ src/types/MButton.d.ts
@@ -0,0 +1,17 @@
+import { MColorVariantBase } from "./MBase";
+
+export type MButtonType = "button" | "submit" | "reset";
+
+export interface MButtonVariantBase extends MColorVariantBase {
+    link: unknown;
+    "outline-primary": unknown;
+    "outline-secondary": unknown;
+    "outline-success": unknown;
+    "outline-danger": unknown;
+    "outline-warning": unknown;
+    "outline-info": unknown;
+    "outline-light": unknown;
+    "outline-dark": unknown;
+}
+
+export type MButtonVariant = keyof MButtonVariantBase;
