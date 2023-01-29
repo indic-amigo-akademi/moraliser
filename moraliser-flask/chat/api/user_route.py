9a46eee1531c19840258dd53d9e1440cae4509b4
Purbayan Chowdhury <pur.cho.99@gmail.com>
2023-01-29 13:44:05 +0530

    Csrf route and some url fix

--- moraliser-flask/chat/api/user_route.py
+++ moraliser-flask/chat/api/user_route.py
@@ -3,9 +3,10 @@ from chat.api import api_bp
 from chat.forms import LoginForm, RegisterForm
 from chat.models import db, User
 from flask_login import login_user, logout_user, current_user
-from flask_wtf.csrf import generate_csrf
+from flask_wtf.csrf import generate_csrf, validate_csrf
 
-@api_bp.route('/current', methods=['GET'])
+
+@api_bp.route("/current", methods=["GET"])
 def get_current_user():
     user = None
     message = "No current user found!"
@@ -13,70 +14,96 @@ def get_current_user():
     if current_user.is_active:
         user = current_user.serialize
         message = "Current user found!"
-                
-    return jsonify({
-        "success": success,
-        "message": message,
-        "data": {
-            "user": user,
-            "csrf_token": generate_csrf()
+
+    return jsonify(
+        {
+            "success": success,
+            "message": message,
+            "data": {"user": user, "csrf_token": generate_csrf()},
         }
-    })
+    )
 
 
-@api_bp.route('/login', methods=['POST'])
+@api_bp.route("/my-csrf", methods=["GET"])
+def get_csrf_token():
+    return jsonify(
+        {
+            "success": True,
+            "message": "CSRF Token is here!",
+            "data": {"csrf_token": generate_csrf()},
+        }
+    )
+
+
+@api_bp.route("/validate-csrf", methods=["GET"])
+def validate_csrf_token():
+    csrf_token = request.args.get("csrf_token")
+    try:
+        validate_csrf(csrf_token)
+        return jsonify(
+            {"success": True, "message": "CSRF Token is valid", "data": None}
+        )
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e), "data": None})
+
+
+@api_bp.route("/login", methods=["POST"])
 def user_login():
     try:
         form = LoginForm()
 
         if form.validate_on_submit():
-            attempted_user = User.query.filter_by(
-                username=form.username.data).first()
+            attempted_user = User.query.filter_by(username=form.username.data).first()
 
             if attempted_user and attempted_user.check_password(
-                    attempted_password=form.password.data):
+                attempted_password=form.password.data
+            ):
                 login_user(attempted_user)
 
-                return jsonify({
-                    "success": True,
-                    "message": "Logged in successfully!",
-                    "data": None
-                })
+                return jsonify(
+                    {
+                        "success": True,
+                        "message": "Logged in successfully!",
+                        "data": None,
+                    }
+                )
             else:
-                return jsonify({
+                return jsonify(
+                    {
+                        "success": False,
+                        "message": "Error during logging!",
+                        "data": {
+                            "errors": {
+                                "all": [
+                                    "Username and password are not match! Please try again."
+                                ]
+                            }
+                        },
+                    }
+                )
+        if form.errors != {}:
+            return jsonify(
+                {
                     "success": False,
                     "message": "Error during logging!",
                     "data": {
-                        "errors": {
-                            "all": [
-                                "Username and password are not match! Please try again."
-                            ]
-                        }
-                    }
-                })
-        if form.errors != {}:
-            return jsonify({
-                "success": False,
-                "message": "Error during logging!",
-                "data": {
-                    "errors": form.errors,
+                        "errors": form.errors,
+                    },
                 }
-            })
+            )
 
     except Exception as e:
-        current_app.logger.error('>>> Error {}'.format(e))
-        return jsonify({
-            "success": False,
-            "message": "Error during logging!",
-            "data": {
-                "errors": {
-                    "all": [str(e)]
-                }
+        current_app.logger.error(">>> Error {}".format(e))
+        return jsonify(
+            {
+                "success": False,
+                "message": "Error during logging!",
+                "data": {"errors": {"all": [str(e)]}},
             }
-        })
+        )
 
 
-@api_bp.route('/register', methods=['POST'])
+@api_bp.route("/register", methods=["POST"])
 def user_register():
     try:
         form = RegisterForm()
@@ -86,46 +113,41 @@ def user_register():
             username = form.username.data
             password = form.password.data
             phone = form.phone.data
-            newUser = User(email=email,
-                           username=username,
-                           password=password,
-                           phone=phone)
+            newUser = User(
+                email=email, username=username, password=password, phone=phone
+            )
             db.session.add(newUser)
             db.session.commit()
             login_user(newUser)
-            return jsonify({
-                "success": True,
-                "message": "Registered successfully!",
-                "data": None
-            })
+            return jsonify(
+                {"success": True, "message": "Registered successfully!", "data": None}
+            )
 
         if form.errors != {}:
-            return jsonify({
-                "success": False,
-                "message": "Error during registration!",
-                "data": {
-                    "errors": form.errors,
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Error during registration!",
+                    "data": {
+                        "errors": form.errors,
+                    },
                 }
-            })
+            )
 
     except Exception as e:
-        current_app.logger.error('>>> Error {}'.format(e))
-        return jsonify({
-            "success": False,
-            "message": "Error during registration!",
-            "data": {
-                "errors": {
-                    "all": [str(e)]
-                }
+        current_app.logger.error(">>> Error {}".format(e))
+        return jsonify(
+            {
+                "success": False,
+                "message": "Error during registration!",
+                "data": {"errors": {"all": [str(e)]}},
             }
-        })
+        )
 
 
-@api_bp.route('/logout', methods=['POST'])
+@api_bp.route("/logout", methods=["POST"])
 def user_logout():
     logout_user()
-    return jsonify({
-        "success": True,
-        "message": "Logged out successfully!",
-        "data": None
-    })
+    return jsonify(
+        {"success": True, "message": "Logged out successfully!", "data": None}
+    )
--- moraliser-vue/src/utils/fetchUtils.ts
+++ moraliser-vue/src/utils/fetchUtils.ts
@@ -21,23 +21,28 @@ export function toHeaders(headersJson: { [key: string]: string | null }) {
   return headers;
 }
 
+export function getUrl(route: string) {
+  const environ = import.meta.env.DEV ? "dev" : "prod";
+  return locationURL[environ].concat(route);
+}
+
 export function fetchData<T>(
   route: string,
   data: { [key: string]: string } | null,
   method: FetchMethodType,
   callback: Function
 ) {
-  const environ = import.meta.env.DEV ? "dev" : "prod";
-
   const csrfToken: string | null = store.state.csrf_token;
-  const headers = {
-    "X-CSRFToken": csrfToken,
-  };
+  const headers = {};
+  //   const headers = {
+  //     "X-CSRF-TOKEN": csrfToken,
+  //   };
 
   //   const URL = window.location.origin.concat(route);
-  const URL = locationURL[environ].concat(route);
+  const URL = getUrl(route);
   if (method === "POST") {
     const formData = new FormData();
+    formData.append("csrf_token", csrfToken as string);
     if (data) for (const i in data) formData.append(i, data[i] || "");
 
     fetch(URL, {
