9a71236a6173a1658ebfe0b8973b7b5065a6f733
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-03-27 11:47:11 +0530

    update 1 file and copy 1 file

--- moraliser-flask/chat/api/chat_route.py
+++ moraliser-flask/chat/api/chat_route.py
@@ -1,57 +1,11 @@
 from flask import request, jsonify
 from chat.api import api_bp
-import pickle
-import re
-
-
-class TextSpamClassifier:
-    def __init__(self) -> None:
-        with open("notebooks/spam_filter/model_spam.pickle", "rb") as handle:
-            self.model = pickle.load(handle)
-        with open("notebooks/spam_filter/vectorizer_spam.pickle", "rb") as handle:
-            self.vectorizer = pickle.load(handle)
-
-    def __preprocess_text(self, text):
-        text = text.lower()
-        text = re.sub(r"[^a-zA-Z0-9]", " ", text)
-        text = text.strip()
-        text = text.split()
-        text = " ".join(list(filter(lambda x: x not in ["", " "], text)))
-        return text
-
-    def predict_proba(self, X):
-        val = self.__preprocess_text(X)
-        val = self.vectorizer.transform([val])
-        prob = self.model.predict_proba(val)[0][1]
-        return prob
-
-
-class TextProfanityClassifier:
-    def __init__(self) -> None:
-        with open("notebooks/profanity_filter/model_profanity.pickle", "rb") as handle:
-            self.model = pickle.load(handle)
-        with open(
-            "notebooks/profanity_filter/vectorizer_profanity.pickle", "rb"
-        ) as handle:
-            self.vectorizer = pickle.load(handle)
-
-    def __preprocess_text(self, text):
-        text = text.lower()
-        text = re.sub(r"[^a-zA-Z0-9]", " ", text)
-        text = text.strip()
-        text = text.split()
-        text = " ".join(list(filter(lambda x: x not in ["", " "], text)))
-        return text
-
-    def predict_proba(self, X):
-        val = self.__preprocess_text(X)
-        val = self.vectorizer.transform([val])
-        prob = self.model.predict(val)[0]
-        return prob
 
 
 @api_bp.route("/text-validate", methods=["POST"])
 def text_chat_validate():
+    from chat.utils.text_classifier import TextSpamClassifier, TextProfanityClassifier
+
     message = request.form.get("message")
     tsc = TextSpamClassifier()
     tpc = TextProfanityClassifier()
